74f51b26f17f8bb6fee7cd8c9efb5107bcd8bf2b
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2026-05-02 09:35:55 +0600

    Add files via upload

---
+++ phase1-core-34/Dynamic Programming/Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+// Problem: Climbing Stairs
+// Platform: LeetCode
+// Link: https://leetcode.com/problems/climbing-stairs/
+// time complexity: O(n)
+// space complexity: O(n)
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        if (n == 1)
+            return 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
