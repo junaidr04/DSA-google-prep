88f727aea31b0b9793953c43884c1dec15a21e7d
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2026-05-02 09:42:00 +0600

    Update Climbing Stairs.cpp

--- phase1-core-34/Dynamic Programming/Climbing Stairs.cpp
+++ phase1-core-34/Dynamic Programming/Climbing Stairs.cpp
@@ -21,3 +21,25 @@ public:
         return dp[n];
     }
 };
+
+// TC: O(n) - we need to calculate the number of ways to climb n stairs, which requires us to fill the dp array up to n.
+// SC: O(1) - we can optimize the space complexity to O(1) by only keeping track of the last two values instead of the entire dp array.
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n == 1)
+            return 1;
+        int prev2 = 1;
+        int prev1 = 2;
+        for (int i = 3; i <= n; i++)
+        {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
