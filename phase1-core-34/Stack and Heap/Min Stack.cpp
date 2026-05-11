261f50557cac06256411c37dbcb107b549081618
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2026-05-11 09:00:42 +0600

    Add files via upload

---
+++ phase1-core-34/Stack and Heap/Min Stack.cpp
@@ -0,0 +1,54 @@
+// Problem: Min Stack
+// Platform: LeetCode
+// Link: https://leetcode.com/problems/min-stack/description/
+// time complexity: O(1)
+// space complexity: O(n)
+
+class MinStack
+{
+    stack<int> st;
+    stack<int> minSt;
+
+public:
+    MinStack()
+    {
+    }
+
+    void push(int val)
+    {
+        st.push(val);
+        if (minSt.empty())
+        {
+            minSt.push(val);
+        }
+        else
+        {
+            minSt.push(min(val, minSt.top()));
+        }
+    }
+
+    void pop()
+    {
+        st.pop();
+        minSt.pop();
+    }
+
+    int top()
+    {
+        return st.top();
+    }
+
+    int getMin()
+    {
+        return minSt.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
