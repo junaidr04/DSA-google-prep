eafbdaeba52b52e2ede1e37085db5026063908f6
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2026-05-20 13:07:10 +0600

    Add files via upload

---
+++ NeetCode-150/Stack/Valid Parentheses.cpp
@@ -0,0 +1,37 @@
+// Problem: Valid Parentheses
+// Platform: LeetCode
+// Link: https://neetcode.io/problems/validate-parentheses/question
+// time complexity: O(n)
+// space complexity: O(n)
+
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<int> st;
+        for (char c : s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if (st.empty())
+                    return false;
+                char top = st.top();
+                if ((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '['))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
